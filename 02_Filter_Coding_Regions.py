3735fab22b75683cc19d715fe7204f06d1c28267
heernaik014 <heernaik016@gmail.com>
2026-07-27 07:36:13 -0700

    Create 02_Filter_Coding_Regions.py

---
+++ 02_Filter_Coding_Regions.py
@@ -0,0 +1,13 @@
+# File: 02_Filter_Coding_Regions.py
+a = 0.45
+b = 0.81
+c = 0.51
+d = 0.33
+if a > 0.50:
+    print("1 to 300 is coding with score", a)
+if b > 0.50:
+    print("301 to 600 is coding with score", b)
+if c > 0.50:
+    print("601 to 900 is coding with score", c)
+if d > 0.50:
+    print("901 to 1200 is coding with score", d)
